45f86c762e6bcedf86f04ef1a536c3a8f555e705
Chris Diewtragulchai <44313901+Diewt@users.noreply.github.com>
2021-04-26 21:01:09 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='FinalProj-BlackJack',
+    version='1',
+    packages=[''],
+    url='https://github.com/Diewt/FinalProj-BlackJack',
+    license='',
+    author='chris',
+    author_email='chrisdiewtragulchai@gmail.com',
+    description='Blackjack game in console'
+)
